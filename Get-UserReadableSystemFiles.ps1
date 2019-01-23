ea424f6075cff81f12b42c68782be22d3300e71e
07am <45973472+07am@users.noreply.github.com>
2019-01-23 01:24:21 -0500

    Update Get-UserReadableSystemFiles.ps1

--- Get-UserReadableSystemFiles.ps1
+++ Get-UserReadableSystemFiles.ps1
@@ -6,5 +6,5 @@
 # This script will generate a decent amount of errors from denied access attempts. Use $ErrorActionPreference='SilentlyContinue' to suppress errors beforehand if ERRORS bother you. $ErrorActionPreference='Continue' to restore to default.
 
 Function Get-UserReadableSystemFiles{
-$ErrorActionPreference='SilentlyContinue';gci "$ENV:windir\system32" -recurse |? {$_.PSISContainer -ne 'true'} | %{get-acl $_.FullName} | ? {$_.AccessToString -like "*USERS*ALLOW*READ*" -and $_.Owner -like "*SYSTEM*"}  | ft path -autosize; $ErrorActionPreference='Continue'
+$ErrorActionPreference='SilentlyContinue';gci "$ENV:windir\system32" -recurse |? {$_.PSISContainer -ne 'true'} | %{get-acl $_.FullName} | ? {$_.AccessToString -like "*USERS*ALLOW*READ*" -and $_.Owner -like "*SYSTEM*"}  | ft path -wrap; $ErrorActionPreference='Continue'
 }
