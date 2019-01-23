ff1f10553c53ab3d78e5683aa6d1eb4ebf10878e
07am <45973472+07am@users.noreply.github.com>
2019-01-23 02:02:08 -0500

    Update and rename Get-UserReadableSystemFiles.ps1 to Get-RSystemFiles.ps1

---
+++ Get-RSystemFiles.ps1
@@ -0,0 +1,10 @@
+# Get-RSystemFiles.ps1
+# One-Liner to Enumerate Windows System32 Directory for Files owned by SYSTEM but readable by USERS.
+##Author @07am
+#
+
+# This script will generate a decent amount of errors from denied access attempts. Uses $ErrorActionPreference='SilentlyContinue' to suppress errors beforehand and $ErrorActionPreference='Continue' to restore to default after.
+
+Function Get-RSystemFiles{
+$ErrorActionPreference='SilentlyContinue';gci "$ENV:windir\system32" -recurse |? {$_.PSISContainer -ne 'true'} | %{get-acl $_.FullName} | ? {$_.AccessToString -like "*USERS*ALLOW*READ*" -and $_.Owner -like "*SYSTEM*"}  | ft path; $ErrorActionPreference='Continue'; Write-Output "Module Complete."
+}
